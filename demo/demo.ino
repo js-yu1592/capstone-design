17d1792c6ef392d54d7effe882aa1c924e7240e7
sunghong32 <35060242+sunghong32@users.noreply.github.com>
2020-06-27 23:49:54 +0900

    Add files via upload

---
+++ demo/demo.ino
@@ -0,0 +1,59 @@
+#include <SoftwareSerial.h>
+#define calibration_factor -7050.0
+#include "HX711.h" //HX711로드셀 엠프 관련함수 호출
+#define DOUT  4 //엠프 데이터 아웃 핀 넘버 선언
+#define CLK  3  //엠프 클락 핀 넘버 
+HX711 scale(DOUT, CLK); //엠프 핀 선언 
+
+
+ 
+int Tx=6; //전송
+int Rx=7; //수신
+
+
+SoftwareSerial btSerial(Tx, Rx);
+
+void setup() 
+{    
+  Serial.begin(9600);
+  btSerial.begin(9600);
+
+
+  scale.set_scale(calibration_factor);  //스케일 지정 
+  scale.tare();  //스케일 설정  
+}
+
+void detection() {
+    Serial.print("입질 감지중.. ");
+    Serial.print(scale.get_units() * 0.453592, 1);  //무제 출력 
+    Serial.print("kg"); //단위
+    Serial.println();
+}
+
+
+
+void loop()
+{
+ 
+    int temp = scale.get_units() * 0.453592;
+ 
+    // 문자열 종료 표시
+    if(scale.get_units() * 0.453592 > -3) { 
+     
+       while(1){
+        detection();
+     
+        if(btSerial.available()){
+          if(scale.get_units() * 0.453592 < -3){
+          btSerial.println(temp);
+        
+         
+         
+         break;
+        }
+       }
+       
+
+    }
+  }  
+}
